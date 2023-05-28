d7bbe80663c4fbead80564b6eefd3a5a33b07874
Aleksey Yudin <heylecs@gmail.com>
2023-05-28 16:32:55 +0300

    1.1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+
+// Burger Menu
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', () => {
+    menuMobile.classList.toggle('menu--open');
+});
+
+
+// Slider
 const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 2,
